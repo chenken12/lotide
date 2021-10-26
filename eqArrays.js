b5356eb137965b60343c3f93ab69c47b99f63daa
Kenneth Chen <kenchen0606@gmail.com>
2021-10-26 21:30:25 +0000

    added eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,36 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    //console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  } else {
+    //console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return false;
+  }
+  //console.assert(actual ===  expected, actual + " !== " + expected);
+};
+
+const eqArrays = function(arr1, arr2) {
+  let passOrFail = (arr1.length === arr2.length? true : false);
+
+  for (let i = 0; i < arr1.length; i++) {
+    passOrFail = assertEqual(arr1[i], arr2[i]);
+    if (!passOrFail) { break; }
+    //console.log(passOrFail, arr1[i], arr2[i]);
+  }
+  if (passOrFail) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+    return true;
+  } else {
+    console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+      return false;
+  }
+  
+};
+
+// TEST CODE
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
