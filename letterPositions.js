56fbe509980e755536ebcae4ed7ef9113cebcff6
Kenneth Chen <kenchen0606@gmail.com>
2021-10-27 21:06:03 +0000

    skip space letterP

--- letterPositions.js
+++ letterPositions.js
@@ -22,15 +22,17 @@ const assertArraysEqual = function(arr1, arr2) {
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
-    if (results[sentence[i]]) {
+    if (sentence[i] === ' ') {
+      continue;
+    } else if (results[sentence[i]]) {
       results[sentence[i]].push(i);
     } else {
       results[sentence[i]] = [i];
     }
   }
-  //console.log(results);
+  console.log(results);
   return results;
 };
 
-letterPositions("compass");
+letterPositions("lighthouse in the house");
 assertArraysEqual(letterPositions("hello").e, [1]);
