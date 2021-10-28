78c4fc25d4303c8123761c68ad964c768a23e3ec
Kenneth Chen <kenchen0606@gmail.com>
2021-10-28 20:48:59 +0000

    add findkey

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  //console.assert(actual ===  expected, actual + " !== " + expected);
+};
+
+const findKey = function(list, callb) {
+  for (const key in list) {
+    if(callb(list[key])) {
+      console.log(key);
+      return key;
+    }
+  }
+};
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(result, "noma");
