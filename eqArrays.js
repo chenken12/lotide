a8079d26d87d4aa5879087fbf36488308811e8fa
Kenneth Chen <kenchen0606@gmail.com>
2021-10-30 01:15:07 +0000

    Recursive eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -14,11 +14,13 @@ const eqArrays = function(arr1, arr2) {
   let passOrFail = (arr1.length === arr2.length ? true : false);
 
   for (let i = 0; i < arr1.length; i++) {
-    passOrFail = assertEqual(arr1[i], arr2[i]);
-    if (!passOrFail) {
-      break;
+    if (Array.isArray(arr1[i])) {
+      passOrFail = eqArrays(arr1[i], arr2[i]);
+    } else if (!assertEqual(arr1[i], arr2[i])){
+      passOrFail = false;
     }
   }
+
   if (passOrFail) {
     //console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
     return true;
@@ -30,10 +32,16 @@ const eqArrays = function(arr1, arr2) {
 };
 
 // TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+let t1 = eqArrays([1, 2, 3], [1, 2, 3]); // => true
+let t2 = eqArrays([1, 2, 3], [3, 2, 1]); // => false
+console.log(`t1: ${t1} t2: ${t2}`);
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+
+let a = eqArrays([[2, 3], [4]], [[2, 3], [4]]); // => true
+let b = eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]); // => false
+let c = eqArrays([[2, 3], [4]], [[2, 3], 4]); // => false
+console.log(`a: ${a} b: ${b} c: ${c}`);
