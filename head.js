ba334a557f0d4bb4850b2fbfdaa99628d2aa6786
Kenneth Chen <kenchen0606@gmail.com>
2021-11-01 20:23:29 +0000

    Refactor assertEqual and head

--- head.js
+++ head.js
@@ -1,16 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-  //console.assert(actual ===  expected, actual + " !== " + expected);
-};
-
 const head = function(arr1) {
   return arr1[0];
 };
 
-assertEqual(head([]), 5);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
